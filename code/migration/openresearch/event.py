df9d40cbb4ac71de5feab09378073121cbf05aa1
Wolfgang Fahl <wf@bitplan.com>
2021-04-06 16:29:45 +0200

    adds event as a start for #155

---
+++ code/migration/openresearch/event.py
@@ -0,0 +1,60 @@
+'''
+Created on 2021-04-06
+
+@author: wf
+'''
+
+class Event(object):
+    '''
+    I represent an Event
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
+    @classmethod
+    def getSamples(cls):
+        samplesLOD=[{
+            "acronym":"ICSME 2020",
+            "ordinal": 36,
+            "type": "Conference",
+            "subject": "Software engineering"
+        }]
+        return samplesLOD
+    
+    @classmethod
+    def getSampleWikiSon(cls,mode='legacy'):
+        if mode=='legacy':
+            samplesWikiSon=["""{{Event
+|Acronym=ICSME 2020
+|Title=36th IEEE International Conference on Software Maintenance and Evolution
+|Series=ICSME
+|Type=Conference
+|Field=Software engineering
+|Start date=2020/09/27
+|End date=2020/10/03
+|Homepage=https://icsme2020.github.io/
+|City=Adelaide
+|State=Online
+|Country=Australia
+|Abstract deadline=2020/05/22
+|Paper deadline=2020/05/28
+|Notification=2020/08/04
+|Camera ready=2020/08/25
+|Has host organization=Institute of Electrical and Electronics Engineers
+|Has coordinator=Sebastian Baltes
+|has general chair=Christoph Treude, Hongyu Zhang
+|has program chair=Kelly Blincoe, Zhenchang Xing
+|has demo chair=Mario Linares Vasquez, Hailong Sun
+}}"""]
+        else:
+            samplesWikiSon="..."
+        
+        return samplesWikiSon
+            
+        
+    
+        
