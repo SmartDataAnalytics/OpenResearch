eb62deba3718604aedf964b5d34b497aa83c1840
tholzheim <>
2021-04-13 13:14:14 +0200

    Fixed import

--- migration/migrate/toolbox.py
+++ migration/migrate/toolbox.py
@@ -1,6 +1,6 @@
 from dateutil import parser
 import os
-from migrate.dictionary import Dictionary
+from migration.migrate.Dictionary import Dictionary
 from wikibot.wikiuser import WikiUser
 from wikibot.wikiclient import WikiClient
 import getpass
