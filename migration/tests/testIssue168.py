3ce199f101eb3f158529da75db6c68c30d7a642f
Wolfgang Fahl <wf@bitplan.com>
2021-04-15 16:04:33 +0200

    tries linking events and series

--- migration/tests/testIssue168.py
+++ migration/tests/testIssue168.py
@@ -5,8 +5,10 @@ Created on 2021-04-13
 '''
 import unittest
 from ormigrate.toolbox import HelperFunctions as hf
-from openresearch.event import Event,EventList
+from openresearch.event import Event,EventList,EventSeriesList
 from collections import Counter
+from wikibot.wikiuser import WikiUser
+from pandas.tests.groupby.test_index_as_string import series
 
 class TestIssue168(unittest.TestCase):
     '''
@@ -20,16 +22,35 @@ class TestIssue168(unittest.TestCase):
 
     def tearDown(self):
         pass
+    
+    def getEventsWithSeries(self,wikiUser):
+        eventList=EventList()
+        eventList.fromCache(wikiUser)
+        eventSeriesList=EventSeriesList()
+        eventSeriesList.fromCache(wikiUser)
+        seriesLookup=eventList.getLookup("inEventSeries", withDuplicates=True)
+        seriesAcronymLookup=eventSeriesList.getLookup("acronym",withDuplicates=True)
+        for seriesAcronym in seriesLookup.keys():
+            if seriesAcronym in seriesAcronymLookup:
+                seriesEvents=seriesLookup[seriesAcronym]
+                print(f"{seriesAcronym}:{len(seriesEvents):4d}" )
+            else:
+                print(f"Event Series Acronym {seriesAcronym} lookup failed")
+        if self.debug:
+            print ("%d events/%d eventSeries -> %d linked" % (len(eventList.getList()),len(eventSeriesList.getList()),len(seriesLookup)))
+        return eventList,eventSeriesList
 
+
+    def testEventsWithSeries(self):
+        wikiUser=hf.getSMW_WikiUser(save=hf.inPublicCI())
+        eventList,eventSeriesList=self.getEventsWithSeries(wikiUser)
 
     def testRatingCallback(self):
         '''
         test the rating call back
         '''
         wikiUser=hf.getSMW_WikiUser(save=hf.inPublicCI())
-        eventList=EventList()
-        eventList.fromCache(wikiUser)
-        print (len(eventList.getList()))
+        eventList,eventSeriesList=self.getEventsWithSeries(wikiUser)
         lod=eventList.getRatedLod(Event.rateMigration)
         counter=Counter()
         for record in lod:
