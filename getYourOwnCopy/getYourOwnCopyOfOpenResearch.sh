e43bba66e7d2a2a6f1cb56d348d7aa26988b6787
Wolfgang Fahl <wf@bitplan.com>
2021-07-06 18:19:06 +0200

    adds Mermaid and Diagrams but disables UrlGetParameters for the time being

--- getYourOwnCopy/getYourOwnCopyOfOpenResearch.sh
+++ getYourOwnCopy/getYourOwnCopyOfOpenResearch.sh
@@ -5,13 +5,18 @@
 #
 installAndGetMediaWikiDocker() {
   pip install -U pymediawikidocker
-  mwcluster --forceRebuild --versionList 1.31.15 --smwVersion 3.2.3 \
+  # 1.31.15 does not work with diagrams and Mermaid
+  # 1.35.3 is needed if diagrams should be displayed
+  mwcluster --forceRebuild --versionList 1.35.3 --smwVersion 3.2.3 \
      --basePort 9780 --sqlBasePort 9736 --wikiIdList myor \
      --extensionList "Admin Links"  "BreadCrumbs2" "Cargo" "CategoryTree" \
-      "ConfirmAccount" "ConfirmEdit" "Data Transfer" "Header Tabs" \
-      "ImageMap", "InputBox" "LanguageSelector" "MagicNoCache" "Maps7" "Nuke" "Page Forms" \
+      "ConfirmAccount" "ConfirmEdit" "Data Transfer" "Diagrams" "Header Tabs" \
+      "ImageMap", "InputBox" "LanguageSelector" "MagicNoCache" "Maps7" "Mermaid" "Nuke" "Page Forms" \
       "ParserFunctions" "PDFEmbed" "Renameuser" "Replace Text" "Semantic Result Formats" "SyntaxHighlight" \
-      "TitleBlacklist"  "UrlGetParameters" "Variables" 
+      "TitleBlacklist"   "Variables" \
+      --logo https://confident.dbis.rwth-aachen.de/or/images/f/f2/OpenResearch-Logo-Copy.png
+  #"UrlGetParameters"
+  #
   pip install -U py-3rdparty-mediawiki
   pip install -U OpenResearchMigration
   pip install -U wikirender
@@ -54,8 +59,8 @@ copyWiki() {
   wikipush -s $l_source -t myor -q "[[Template:+]]"
   wikipush -s $l_source -t myor -p "Template:Event" "Template:Event series" "Template:Tablelongrow" "Template:Tablerow" "Template:Tablesection"
   wikipush -s $l_source -t myor -q "[[isA::Event]][[Modification date::>2021]]" --progress -qd 10
-  wikipush -s $l_source -t myor -q "[[Category:Event series]][[Modification date::>2021]]" 
-  wikipush -s $l_source -t myor -p "Template:Col-begin" "Template:Col-1-of-3" "Template:Col-2-of-3" "Template:Col-3-of-3" "Template:Col-end" "Template:Research_field_tpl" "Template:Yearly_calendar" 
+  wikipush -s $l_source -t myor -q "[[Category:Event series]][[Modification date::>2021]]"
+  wikipush -s $l_source -t myor -p "Template:Col-begin" "Template:Col-1-of-3" "Template:Col-2-of-3" "Template:Col-3-of-3" "Template:Col-end" "Template:Research_field_tpl" "Template:Yearly_calendar"
   wikipush -s $l_source -t myor -f -p "Main Page" "MediaWiki:Sidebar"
 }
 
@@ -63,4 +68,3 @@ copyWiki() {
 installAndGetMediaWikiDocker
 setupWikiUser
 copyWiki or
-  
