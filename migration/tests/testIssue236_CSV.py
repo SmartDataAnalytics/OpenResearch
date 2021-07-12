1102024d456eff28d568ae7925915642041ad382
Wolfgang Fahl <wf@bitplan.com>
2021-07-12 09:54:56 +0200

    tries refactoring for #236

--- migration/tests/testIssue236_CSV.py
+++ migration/tests/testIssue236_CSV.py
@@ -8,7 +8,6 @@ from ormigrate.toolbox import HelperFunctions as hf
 from openresearch.eventcorpus import EventCorpus
 from openresearch.event import Event,EventList, EventSeriesList
 from pathlib import Path
-import getpass
 
 class TestIssue236(unittest.TestCase):
     '''
@@ -22,15 +21,6 @@ class TestIssue236(unittest.TestCase):
         self.wikiId='orclone'
         self.backupdir= str(Path.home() / 'wikibackup'/ self.wikiId )
         pass
-    
-    
-    def runInCi(self):
-        '''
-        should these test run in the continuous integration environment
-        '''
-        #runit=  getpass.getuser() != "mk"
-        runit=True
-        return runit
   
     def tearDown(self):
         pass
@@ -41,8 +31,6 @@ class TestIssue236(unittest.TestCase):
         get getting the events in a certain series
         e.g. 3DUI
         '''
-        if hf.inPublicCI():
-            return
         eventCorpus = EventCorpus(debug=self.debug)
         eventCorpus.fromWikiSonBackupFiles(wikiId=self.wikiId,backupdir=self.backupdir)
         eventsLinked=eventCorpus.getEventsInSeries('3DUI')
@@ -52,8 +40,6 @@ class TestIssue236(unittest.TestCase):
         '''
         tests loading of limited events from backup files.
         '''
-        if hf.inPublicCI():
-            return
         eventList2 = EventList()
         eventList2.fromWikiSonBackupFiles('Event', wikiId=self.wikiId,backupdir=self.backupdir,listOfItems=['3DUI 2020', '3DUI 2016'])
         self.assertGreaterEqual(len(eventList2.getList()), 2)
@@ -63,8 +49,6 @@ class TestIssue236(unittest.TestCase):
         """
         Test loading of event series and eventlist from backup files.
         """
-        if hf.inPublicCI():
-            return
         eventSeriesList=EventSeriesList()
         eventSeriesList.fromWikiSonBackupFiles('Event series',wikiId=self.wikiId,backupdir=self.backupdir)
         # self.assertGreater(len(eventSeriesList.getList()), 100)
@@ -79,8 +63,6 @@ class TestIssue236(unittest.TestCase):
         """
         Test the module for csv generation module in Event Corpus
         """
-        if hf.inPublicCI():
-            return
         eventCorpus=EventCorpus()
         eventCorpus.fromWikiSonBackupFiles(self.backupdir,self.wikiId,['3DUI 2020','3DUI'])
         file = eventCorpus.getEventCsv('3DUI 2020')
@@ -92,8 +74,6 @@ class TestIssue236(unittest.TestCase):
         """
         tests updating an event from EventList
         """
-        if hf.inPublicCI():
-            return
         eventList = EventList()
         eventSamples=Event.getSamples()
         eventList.fromLoD(eventSamples)
@@ -109,7 +89,6 @@ class TestIssue236(unittest.TestCase):
         updatedList = eventList.getList()
         self.assertEqual(updatedList[0].testAttr,'test')
         self.assertEqual(len(updatedList),len(eventSamples))
-
 
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'Test.testName']
