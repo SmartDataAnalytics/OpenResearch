16572f61651a0caf018ce599e4440e57840661c5
tholzheim <tim.holzheim@rwth-aachen.de>
2022-04-07 18:56:58 +0200

    fixed typo
    Signed-off-by: tholzheim <tim.holzheim@rwth-aachen.de>

--- migration/tests/test_PageProvenance.py
+++ migration/tests/test_PageProvenance.py
@@ -15,7 +15,7 @@ class TestPageProvenance(BaseTest):
         """
         tests extracting the pageEditor and pageCreator
         """
-        # oly test if ./or/curators.txt is present
+        # only test if ./or/curators.txt is present
         return
         pageTitle = "AAAI 2021"
         pageProvenance = PageProvenance(wikiId=self.wikiId, pageTitle=pageTitle)
