f7a9c92722da060ee8f7b663b03ccae9290b3f08
Wolfgang Fahl <wf@bitplan.com>
2022-12-21 22:39:46 -0700

    removes 3.11 support duo to missing upstream support in wikibaseintegrator

--- migration/setup.py
+++ migration/setup.py
@@ -32,7 +32,8 @@ setup(name='OpenResearchMigration',
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
             'Programming Language :: Python :: 3.10',
-            'Programming Language :: Python :: 3.11'
+            # wikibase integrator not compatible yet
+            #'Programming Language :: Python :: 3.11'
       ],
       packages=['ormigrate','ormigrate.smw','openresearch',''],
       package_data={'ormigrate': ['resources/*.json','resources/*.yaml','resources/templates/**/*.jinja','resources/topics/*.json']},
